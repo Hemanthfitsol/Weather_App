7ced747db832d6be2b035214ce36b675d17f0f30
Hemanthfitsol <146804316+Hemanthfitsol@users.noreply.github.com>
2025-01-29 23:39:27 +0530

    added weather app code

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c36085f6059a6502e3783fcfd4208ff9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
